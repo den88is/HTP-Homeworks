45b5df01728006d169713a2ab7e19efba2a0066c
Andrei Angurets <andrei.angurets@gmail.com>
2014-12-22 16:21:27 +0300

    homework redone according to comments

--- 07.Collections/aangurets/7.1.RandomNumberGenerator/src/main/java/Main.java
+++ 07.Collections/aangurets/7.1.RandomNumberGenerator/src/main/java/Main.java
@@ -1,32 +1,24 @@
-import java.util.Iterator;
-import java.util.Random;
-import java.util.TreeSet;
+import java.util.*;
 
-public class Main {
-    public Main() {
-    }
-
+public abstract class Main {
     public static void main(String[] args) {
-        TreeSet mySet = new TreeSet();
-        Random rand = new Random();
+        Random random = new Random();
+        int[] numbers = new int[new Random().nextInt(150) + 50];
 
-        Integer num;
-        while(mySet.size() < 50) {
-            num = Integer.valueOf(rand.nextInt(500));
-            mySet.add(num);
+        for (int count = 0; count < numbers.length; count++) {
+            numbers[count] = random.nextInt(1000);
         }
 
-        while(mySet.size() < 200) {
-            num = Integer.valueOf(rand.nextInt(500));
-            mySet.add(num);
+        Set<Integer> collectionOfValues = new HashSet<Integer>();
+
+        for (int c = 0; c < numbers.length; c++) {
+            collectionOfValues.add(numbers[c]);
         }
+        Set<Integer> mySet = new TreeSet<Integer>();
+        mySet.addAll(collectionOfValues);
 
-        Iterator num1 = mySet.iterator();
-
-        while(num1.hasNext()) {
-            Integer i = (Integer)num1.next();
-            System.out.println(i);
+        for (int item : mySet) {
+            System.out.println(item);
         }
-
     }
 }
