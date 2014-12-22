b2fecafc8f9ee9b6b7714721f1d12568458d6bd9
Andrei Angurets <andrei.angurets@gmail.com>
2014-12-22 20:36:33 +0300

    fix

--- 07.Collections/aangurets/7.1.RandomNumberGenerator/src/main/java/Main.java
+++ 07.Collections/aangurets/7.1.RandomNumberGenerator/src/main/java/Main.java
@@ -3,19 +3,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
+        printSet(set2TreeSet(array2Set(fillingTheArray(random))));
+    }
+    public static Integer[] fillingTheArray(Random random) {
         Integer[] numbersArray = new Integer[new Random().nextInt(150) + 50];
-
         for (int g = 0; g < numbersArray.length; g++) {
             numbersArray[g] = random.nextInt(1000);
         }
-
-        Set<Integer> numbersSet = new HashSet<Integer>();
-
-        Collections.addAll(numbersSet, numbersArray);
-
-        TreeSet<Integer> mySet = new TreeSet<Integer>();
-        mySet.addAll(numbersSet);
-
-        System.out.println(mySet);
+        return numbersArray;
     }
-  }
+
+    public static Set<Integer> array2Set(Integer[] array) {
+        Set<Integer> numbersSet = new HashSet<Integer>();
+        Collections.addAll(numbersSet, array);
+        return numbersSet;
+    }
+
+    public static TreeSet<Integer> set2TreeSet(Set<Integer> numberSet) {
+        TreeSet<Integer> mySet = new TreeSet<Integer>();
+        mySet.addAll(numberSet);
+        return mySet;
+    }
+
+    public static void printSet(TreeSet<Integer> name) {
+        System.out.println(name);
+    }
+}
